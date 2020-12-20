0f1ca4afb36a8e69f513990c05d8635bdec03af5
たた <pf35301@gmail.com>
2020-12-20 23:20:49 +0900

    [add] gitignore setting

---
+++ Assets/Scripts/Main/PixelBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Matomaru.Main {
+    public class PixelBuilder : MonoBehaviour {
+
+        [SerializeField]
+        private GameObject textureObject;
+
+        private void OnEnable() {
+            if(textureObject == null) {
+
+            }
+        }
+
+        private void OnValidate() {
+            
+        }
+    }
+}
