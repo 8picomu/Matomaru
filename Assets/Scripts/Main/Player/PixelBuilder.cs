5b430f0e588a5f13a751cfed88d6f52893ad613e
たた <pf35301@gmail.com>
2020-12-23 08:02:15 +0900

    [clean] directory

--- Assets/Scripts/Main/Player/PixelBuilder.cs
+++ Assets/Scripts/Main/Player/PixelBuilder.cs
@@ -6,42 +6,16 @@ using pf35301.Extensions.Editor;
 namespace Matomaru.Main {
     public class PixelBuilder : MonoBehaviour {
 
-        [Header("DotsParent")]
+        [SerializeField]
+        private uint m_CanvasXSize;
+        [SerializeField]
+        private uint m_CanvasYSize;
 
         [SerializeField]
-        private GameObject m_dotsParent;
-
-        [SerializeField, Tag]
-        private string m_dotsTag = "DotsParent";
-
-        [SerializeField, ReadOnly]
-        private string m_dotsParentName = "DotsParent";
-
-        [Header("Dots")]
-
-        [SerializeField]
-        private GameObject m_Dot;
+        private uint m_CanvansIncludingPixel;
 
         private void OnValidate() {
-            if(m_dotsParent == null) {
-                var haveDotsObject = false;
-                foreach(Transform item in transform) {
-                    if(item.CompareTag(m_dotsTag)) {
-                        haveDotsObject = true;
-                        m_dotsParent = item.gameObject;
-                        break;
-                    }
-                }
 
-                if(haveDotsObject == false) {
-                    var obj = new GameObject();
-                    m_dotsParent = Instantiate(obj);
-                }
-
-                m_dotsParent.transform.parent = transform;
-                m_dotsParent.name = m_dotsParentName;
-                m_dotsParent.tag = m_dotsTag;
-            }
         }
 
         private void OnEnable() {
