acf7da5de43537009f0c53908846670e3ba63d53
たた <pf35301@gmail.com>
2020-12-22 14:39:00 +0900

    [clean] firectory

---
+++ Assets/Scripts/Main/Player/PixelAdjuster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Matomaru.Main {
+    public class PixelAdjuster : MonoBehaviour {
+        private Vector3 m_cashPosition;
+
+        private void LateUpdate() {
+            m_cashPosition = transform.localPosition;
+
+            transform.localPosition = new Vector3(
+                Mathf.RoundToInt(m_cashPosition.x), 
+                Mathf.RoundToInt(m_cashPosition.y), 
+                Mathf.RoundToInt(m_cashPosition.z)
+                );
+        }
+
+        private void OnRenderObject() {
+            transform.localPosition = m_cashPosition;
+        }
+    }
+}
---
+++ Assets/Scripts/Main/Player/PlayerMover.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UniRx;
+using pf35301.Extensions.Editor;
+
+namespace Matomaru.Main {
+    
+    class PlayerMover : MonoBehaviour {
+
+        private IInputObservables m_InputObservables;
+
+        [SerializeField, ReadOnly]
+        private Rigidbody2D m_RigidBody;
+
+        [Header("Move")]
+
+        [SerializeField]
+        private float m_MoveGain = 1.0f;
+
+        [SerializeField]
+        private float m_MoveSpeedLimit = 1.0f;
+
+        private void Start() {
+            if(m_RigidBody == null) {
+                m_RigidBody = GetComponent<Rigidbody2D>();
+            }
+
+            if(m_InputObservables == null) {
+                m_InputObservables = ServiceLocatorProvider.Instance.Current.Resolve<IInputObservables>();
+            }
+
+            m_InputObservables.LStickObservable.Subscribe(
+                vec => {
+                    m_RigidBody.AddForce(vec * m_MoveGain);
+                });
+        }
+    }
+}
