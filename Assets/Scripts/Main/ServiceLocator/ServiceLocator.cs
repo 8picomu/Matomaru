fa5d4db9dde91ea94592f35ea083e2f359fa8e7d
たた <pf35301@gmail.com>
2020-12-22 13:55:19 +0900

    [add] ServiceLocatorPattern

---
+++ Assets/Scripts/Main/ServiceLocator/ServiceLocator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matomaru.Main {
+    public class ServiceLocator {
+        private Dictionary<Type, object> container = new Dictionary<Type, object>();
+
+        public T Resolve<T>() {
+            return (T)container[typeof(T)];
+        }
+
+        public void Register<T>(T instance) {
+            container.Add(typeof(T), instance);
+        }
+    }
+}
---
+++ Assets/Scripts/Main/ServiceLocator/ServiceLocatorProvider.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Matomaru.Main {
+    class ServiceLocatorProvider : SingletonMonoBehaviour<ServiceLocatorProvider> {
+        public ServiceLocator Current { get; private set; }
+
+        private void Awake() {
+            Current = new ServiceLocator();
+        }
+    }
+}
