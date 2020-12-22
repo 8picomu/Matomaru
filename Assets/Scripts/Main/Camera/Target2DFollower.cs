e1158a015a8ed308ad764678e855c67ed54ac837
たた <pf35301@gmail.com>
2020-12-22 14:39:38 +0900

    [add] target2dfollower

---
+++ Assets/Scripts/Main/Camera/Target2DFollower.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Matomaru.Main {
+    class Target2DFollower : MonoBehaviour {
+        [SerializeField]
+        private GameObject m_Target;
+
+        private void FixedUpdate() {
+            if(m_Target != null) {
+                var pos = m_Target.transform.position;
+                transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+            } 
+        }
+    }
+}
