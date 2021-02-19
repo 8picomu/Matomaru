a41f64d365cbbdc3908cd20291f974ec88a916d3
たた <pf35301@gmail.com>
2021-02-19 10:08:56 +0900

    [add] ECS_dots test

--- Assets/DOTS/Scripts/Main/Pixel/ComponentSystem/MovePixelSystem.cs
+++ Assets/DOTS/Scripts/Main/Pixel/ComponentSystem/MovePixelSystem.cs
@@ -1,19 +1,25 @@
-﻿using Unity.Entities;
+﻿using Unity.Transforms;
+using Unity.Entities;
 using Unity.Physics;
 using Unity.Jobs;
 
+using UnityEngine;
+using Matomaru.Main;
+
 namespace Matomaru.ECS.Main {
-    class MovePixelSystem : ComponentSystem {
-
-        protected override void OnCreate() {
-
-        }
+    class MovePixelSystem : SystemBase {
 
         protected override void OnUpdate() {
+
+            var pos = Camera.main.ScreenToWorldPoint(InputProvider.MousePosition);
+
             Entities
-                .ForEach((ref PhysicsVelocity physicsVelosity) => {
-                physicsVelosity.Linear.x = 10.0f;
-            });
+                .ForEach((ref PhysicsVelocity physicsVelosity, in Translation translation) => {
+                    var value = Random.Range(1.0f, 10.0f);
+
+                    physicsVelosity.Linear.x = (pos.x - translation.Value.x) * value;
+                    physicsVelosity.Linear.y = (pos.y - translation.Value.y) * value;
+            }).Run();
         }
     }
 }
